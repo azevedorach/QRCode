f562c63074d110d711c89ef08e748e220aeb25a3
RAQUEL SALES DE AZEVEDO <raquel.sales.azevedo@hotmail.com>
2020-05-15 23:39:48 -0300

    Adicionar arquivos de projeto.

---
+++ QR_CODE.Android/App.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+
+namespace QR_CODE.TestZXing
+{
+    public class App : Application
+    {
+        public App()
+        {
+            Button btnAbrirTelaScanner = new Button() { Text = "Abrir tela de Scanner QR Code" };
+            btnAbrirTelaScanner.Clicked += async (sender, e) => 
+            {
+                await Application.Current.MainPage.Navigation.PushAsync(new PaginaScannerQRCode());
+            };
+
+            //Página principal da sua aplicação
+            var novaPagina = new ContentPage
+            {
+                Title = "Teste Scanner QR Code",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Este é um teste do Scanner QR Code!"
+                        }, btnAbrirTelaScanner
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(novaPagina);
+        }
+
+    }
+}
---
+++ QR_CODE/PaginaScannerQRCode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace QR_CODE
+{
+    public class PaginaScannerQRCode : ContentPage
+    {
+        ZXingScannerPage paginaScannerQRCode;
+        Button buttonLeituraQRCode;
+
+        public PaginaScannerQRCode() : base()
+        {
+            buttonLeituraQRCode = new Button
+            {
+                Text = "Leitura de QR Code",
+                AutomationId = "btnLeituraQRCode",
+            };
+            buttonLeituraQRCode.Clicked += async delegate
+            {
+                paginaScannerQRCode = new ZXingScannerPage();
+                paginaScannerQRCode.OnScanResult += (result) =>
+                {
+                    paginaScannerQRCode.IsScanning = false;
+
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        Navigation.PopAsync();
+                        DisplayAlert("QR Code lido: ", result.Text, "OK");
+                    });
+                };
+
+                await Navigation.PushAsync(paginaScannerQRCode);
+            };
+
+            var posicaoButaoPaginaScanner = new StackLayout();
+            posicaoButaoPaginaScanner.Children.Add(buttonLeituraQRCode);
+
+            Content = posicaoButaoPaginaScanner;
+        }
+    }
+}
